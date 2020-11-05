9743498706308561c5ae51916b9bafd1a3fb62f5
Phillip Phoelich <pphoelich@gmail.com>
2020-11-05 20:40:51 +0100

    Created some APIs for fetching all entries and a specific entry based on ID. Made the list entries link to the correct page.

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,28 +1,26 @@
-import Head from 'next/head'
-import {  FC } from 'react'
-import styles from '../styles/components/Page.module.css'
+import Head from "next/head";
+import { FC } from "react";
+import styles from "../styles/components/Page.module.css";
 
 export interface PageProps {
-    title: string;
-    tabTitle?: string;
+  title: string;
+  tabTitle?: string;
 }
 
-const Page: FC<PageProps> = ({children, title, tabTitle}) => {
+const Page: FC<PageProps> = ({ children, title, tabTitle }) => {
   return (
     <div className={`${styles.page} ${styles.dark}`}>
       <Head>
-        <title>{ tabTitle || title }</title>
+        <title>{tabTitle || title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-          <h1>{title}</h1>
-        </header>
-      <main className={styles.main}>
-        {children}
-      </main>
+        <h1>{title}</h1>
+      </header>
+      <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>Made with ☕ by Phillip</footer>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
-import Page from '../components/Page';
+import { useCallback, useEffect, useState } from "react";
+import Link from "next/link";
+import NarrowContainer from "../components/NarrowContainer";
+import Page from "../components/Page";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/entries")
+      .then((result) => result.json())
+      .then((entryData) => entryData.entries)
+      .then((entries) => {
+        setEntries(entries);
+      });
+  }, []);
+
   return (
     <Page title="日本語 Grammar Dictionary" tabTitle="日本語 Grammar Dictionary">
-      <div id="search-area">
-        <input type="text" placeholder="Search here" />
-      </div>
-      <div id="result-area">
-        { [1, 2, 3, 4, 5, 6].map(res => 
-          <div key={res} className="result">RESULT {res}</div>
-        )}
-      </div>
+      <NarrowContainer>
+        <div id="search-area" className={styles.searchArea}>
+          <input type="text" placeholder="Search here" />
+        </div>
+        <div id="entry-list" className={styles.entryList}>
+          {entries.map((entry) => (
+            <Link key={entry.id} href={`/entry/${encodeURIComponent(entry.id)}`}>
+              <div className={styles.entry}>
+                {entry.title.ja} {entry.title.da}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </NarrowContainer>
     </Page>
-  )
+  );
 }
