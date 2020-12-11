ee8fe620aab6d5554f4f8b4059107f7e3996add3
Phillip Phoelich <pphoelich@gmail.com>
2020-12-11 23:27:41 +0100

    Added meta tags and links for PWA in head.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,12 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="apple-touch-icon" href="public/icons/apple-icon-180.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="日本語 Grammar Dictionary" />
+        <meta name="apple-mobile-web-app-title" content="日本語 Grammar Dictionary" />
+        <meta name="msapplication-starturl" content="/" />
       </Head>
       <Component {...pageProps} />
     </>
