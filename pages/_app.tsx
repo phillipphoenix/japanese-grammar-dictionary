f68c290d1cc8cfffe260731bda2286b10748d054
Phillip Phoelich <pphoelich@gmail.com>
2020-12-11 23:11:45 +0100

    Made the website into a PWA which should be installable.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="public/icons/apple-icon-180.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
