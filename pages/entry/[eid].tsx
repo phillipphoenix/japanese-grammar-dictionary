74e6d08447015ea133d08c70e6c796529292b035
Phillip Phoelich <pphoelich@gmail.com>
2020-12-11 23:24:51 +0100

    Added a back icon.

--- pages/entry/[eid].tsx
+++ pages/entry/[eid].tsx
@@ -1,58 +1,26 @@
 import Link from "next/link";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
-import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
 import styles from "../../styles/[eid].module.css";
 import NarrowContainer from "../../components/NarrowContainer";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import Page from "../../components/Page";
-import { EntryDto } from "../../types/api/entryDto";
 import EntryCard from "../../components/EntryCard";
-import { Card, CardBody, CardHeader, CardSkeleton } from "../../components/Card";
+import { Card, CardBody, CardHeader } from "../../components/Card";
 import { getEntry } from "../api/entry/[eid]";
 import { getEntries } from "../api/entries";
+import ArrowLeft from "../../svgs/ArrowLeft";
 
 const Entry = ({ entry }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const [entry, setEntry] = useState<EntryDto>(null);
-
-  // useEffect(() => {
-  //   const { eid } = router.query;
-  //   // Only fetch data, when query param is properly set.
-  //   if (!eid) {
-  //     return;
-  //   }
-  //   setIsLoading(true);
-  //   fetch(`/api/entry/${eid}`)
-  //     .then((result) => result.json())
-  //     .then((entry: EntryDto) => {
-  //       console.log("EntryData:", entry);
-  //       setEntry(entry);
-  //       setIsLoading(false);
-  //     });
-  // }, [router]);
-
   return (
     <Page title="日本語 Grammar Entry" tabTitle="Entry: eid">
       <NarrowContainer>
         <div>
           <Link href="/">
-            <a>Back</a>
+            <a>
+              <ArrowLeft className={styles.textSvgs} /> Back
+            </a>
           </Link>
         </div>
-        {isLoading && (
-          <div>
-            {/* <h3>Loading entry...</h3> */}
-            <SkeletonTheme color="#404040" highlightColor="#696969">
-              <h1>
-                <Skeleton />
-              </h1>
-              <Skeleton count={3} />
-            </SkeletonTheme>
-          </div>
-        )}
-        {!isLoading && entry && (
+        {entry && (
           <div className={styles.cardList}>
             <EntryCard
               className={styles.card}
@@ -85,6 +53,7 @@ const Entry = ({ entry }: InferGetStaticPropsType<typeof getStaticProps>) => {
   );
 };
 
+// Server side render and cache content.
 export const getStaticPaths: GetStaticPaths = async () => {
   const entries = await getEntries();
 
