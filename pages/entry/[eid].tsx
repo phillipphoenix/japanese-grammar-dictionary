3782e0e8bf3bc1d5d7813419c6e14ceeed22244e
Phillip Phoelich <pphoelich@gmail.com>
2020-12-14 18:16:20 +0100

    Updated the entries to allow for fallbacks when a page wasn't generated at build time.

--- pages/entry/[eid].tsx
+++ pages/entry/[eid].tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import styles from "../../styles/[eid].module.css";
 import NarrowContainer from "../../components/NarrowContainer";
 import Page from "../../components/Page";
-import EntryCard from "../../components/EntryCard";
+import EntryCard, { EntryCardSkeleton } from "../../components/EntryCard";
 import { Card, CardBody, CardHeader } from "../../components/Card";
 import { getEntry } from "../api/entry/[eid]";
 import { getEntries } from "../api/entries";
 import ArrowLeft from "../../svgs/ArrowLeft";
 
 const Entry = ({ entry }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { isFallback } = useRouter();
+
   return (
     <Page title="日本語 Grammar Entry" tabTitle="Entry: eid">
       <NarrowContainer>
@@ -20,34 +23,38 @@ const Entry = ({ entry }: InferGetStaticPropsType<typeof getStaticProps>) => {
             </a>
           </Link>
         </div>
-        {entry && (
-          <div className={styles.cardList}>
-            <EntryCard
-              className={styles.card}
-              title={`${entry.title.da}`}
-              descriptors={entry.descriptors.da}
-              descriptionShort={entry.descriptionShort.da}
-            />
-            {entry.examples?.da && (
-              <Card className={styles.card}>
-                <CardHeader>
-                  <h2>Examples</h2>
-                </CardHeader>
-                <CardBody>
-                  {entry.examples.da.map((exmp) => (
-                    <div key={exmp.id} className={styles.example}>
-                      <p>{exmp.sentence}</p>
-                      <p>{exmp.translation}</p>
-                      {!exmp.explanation && (
-                        <p className={styles.explanation}>{exmp.explanation}</p>
-                      )}
-                    </div>
-                  ))}
-                </CardBody>
-              </Card>
-            )}
-          </div>
-        )}
+
+        <div className={styles.cardList}>
+          {isFallback && <EntryCardSkeleton />}
+          {!isFallback && entry && (
+            <>
+              <EntryCard
+                className={styles.card}
+                title={`${entry.title.da}`}
+                descriptors={entry.descriptors.da}
+                descriptionShort={entry.descriptionShort.da}
+              />
+              {entry.examples?.da && (
+                <Card className={styles.card}>
+                  <CardHeader>
+                    <h2>Examples</h2>
+                  </CardHeader>
+                  <CardBody>
+                    {entry.examples.da.map((exmp) => (
+                      <div key={exmp.id} className={styles.example}>
+                        <p>{exmp.sentence}</p>
+                        <p>{exmp.translation}</p>
+                        {!exmp.explanation && (
+                          <p className={styles.explanation}>{exmp.explanation}</p>
+                        )}
+                      </div>
+                    ))}
+                  </CardBody>
+                </Card>
+              )}
+            </>
+          )}
+        </div>
       </NarrowContainer>
     </Page>
   );
@@ -61,7 +68,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
