05e8bc5d0aceb6a2361f1878134f84e7c823da4f
Phillip Phoelich <pphoelich@gmail.com>
2020-12-17 19:12:59 +0100

    Added some credits.

--- pages/credits.tsx
+++ pages/credits.tsx
@@ -12,27 +12,37 @@ const Credits: FC = () => {
           <h2>
             <Link href="/">Go to frontpage</Link>
           </h2>
-          <h2>Icons:</h2>
           <div>
-            Icons made by{" "}
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">
-              Freepik
-            </a>{" "}
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">
-              www.flaticon.com
-            </a>
+            <h2>日本語 Grammar Dictionary:</h2>
+            <p>Created by Phillip Phoelich.</p>
+            <p>
+              The project started late 2020. The main parts of the site were finished during
+              december 2020, but entries are added continuously.
+            </p>
           </div>
           <div>
-            Specific icons are found at{" "}
-            <a
-              href="https://www.flaticon.com/packs/ui-super-basic"
-              title="ui-super-pack"
-              target="_blank"
-            >
-              UI Super Pack
-            </a>
-            .
+            <h2>Icons:</h2>
+            <div>
+              Icons made by{" "}
+              <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">
+                Freepik
+              </a>{" "}
+              from{" "}
+              <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">
+                www.flaticon.com
+              </a>
+            </div>
+            <div>
+              Specific icons are found at{" "}
+              <a
+                href="https://www.flaticon.com/packs/ui-super-basic"
+                title="ui-super-pack"
+                target="_blank"
+              >
+                UI Super Pack
+              </a>
+              .
+            </div>
           </div>
         </div>
       </NarrowContainer>
